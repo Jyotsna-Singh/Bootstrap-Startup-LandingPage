df839ca80fed8ce22dca45fd14d6ae87c2a6a094
Jyotsna-Singh <jyotsna_singh94@yahoo.co.in>
2017-07-04 10:55:12 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
+	  
+	 <!-- Custom CSS -->
+	 <link rel="stylesheet" href="startup.css">
+
+  </head>
+  <body>
+		<nav class="navbar navbar-light bg-faded navbar-fixed-top">
+		  <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#startupnavbar" aria-controls="exCollapsingNavbar2" aria-expanded="false" aria-label="Toggle navigation">
+			&#9776;
+		  </button>
+		   <a class="navbar-brand" href="#"><img src="img/logo.png" height="26"></a>
+		  <div class="collapse navbar-toggleable-xs" id="startupnavbar">
+			
+			<ul class="nav navbar-nav pull-sm-right">
+			  <li class="nav-item active">
+				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#">Features</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#">Pricing</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#">Blog</a>
+			  </li>
+			   <li class="nav-item">
+				<a class="btn btn-primary" href="#">Sign In</a>
+			  </li>
+			</ul>
+		  </div>
+		</nav>
+
+	  
+	 <div id="stage">
+	  	<div id="stage-caption">
+			<h1 class="display-3">That's handy, Harry! Stick it in the Startup.logo</h1>
+		 	<p>I see Startup.logo in your future.</p>
+			<a href="" class="btn btn-lg btn-success">Sign Up Now</a>
+		</div>
+	 </div>
+	  
+	  <section id="feature-one">
+	  	<div class="container">
+		  	<div class="row">
+				<div class="feature-content">
+					<div class="col-lg-6">
+						<iframe width="100%" height="315" src="https://www.youtube.com/embed/ZTLAx3VDX7g" frameborder="0" allowfullscreen></iframe>
+					</div>
+					<div class="col-lg-5 col-lg-offset-1">
+						<h6>Watch this</h6>
+						<h2>Witness the power of Startup.logo in this acapella video.</h2>
+						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper neque quam, at facilisis nisl ornare ut.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper neque quam, at facilisis nisl ornare ut. Ut ut tempor ligula, sed consectetur diam. Aliquam varius aliquam ipsum at gravida. Mauris a mauris finibus, pharetra purus in, imperdiet nisi. Praesent ultricies eu nunc nec cursus. </p>
+					</div>
+				</div>
+			</div>
+		 </div>
+	  
+	  </section>
+    
+	  <section>
+	  <div id="feature-two" class="feature-dark">
+		  <div class="container">
+		  <div class="row">
+			 <div class="feature-content">
+			  <div class="col-lg-6 feature-caption">
+					<h6>Incredible Feature</h6>
+				  	<h2>Totally random websites. At the touch of your finger pads</h2>
+				  	<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper neque quam, at facilisis nisl ornare ut. Ut ut tempor ligula, sed consectetur diam. Aliquam varius aliquam ipsum at gravida. Mauris a mauris finibus, pharetra purus in, imperdiet nisi. Praesent ultricies eu nunc nec cursus. </p>
+				  
+				  	<a href="" role="button" class="btn btn-primary btn-lg">Get it on the action</a>
+				</div>
+				<div class="col-lg-6 text-sm-center hidden-md-down">
+					<img src="img/iphone-1.png" alt="iphone">
+				</div>
+			 </div> 
+		  </div>
+		  </div>
+	   </div>
+	  
+	  </section>
+	  
+	  <section id="feature-three">
+	  	<div class="container">
+		  	<div class="row">
+				<div class="feature-content">
+					<div class="col-lg-6 hidden-md-down">
+						<img src="img/macbook.png" alt="macbook">
+					</div>
+					<div class="col-lg-5 col-lg-offset-1">
+						<h6>the wow factor</h6>
+						<h2>Australians wouldn't give a Startup.logo for anything else.</h2>
+						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper neque quam, at facilisis nisl ornare ut. Ut ut tempor ligula, sed consectetur diam. Aliquam varius aliquam ipsum at gravida. Mauris a mauris finibus, pharetra purus in, imperdiet nisi. Praesent ultricies eu nunc nec cursus. 
+						</p>
+						<a href="" role="button" class="btn btn-primary btn-lg">Get it Done!</a>
+					</div>
+				
+				</div>
+			
+			</div>
+		  
+		</div>
+	  
+	  </section>
+	  
+	  <section id="feature-four" class="feature-alt">
+			<div class="container">
+		    	<div class="row">
+					<div class="feature-content">
+						<div class="col-lg-6">
+								<h6>Kudos</h6>
+								<blockquote class="blockquote">
+								 	&ldquo;No Startup.logo, no kiss. &rdquo;
+									<footer class="blockquote-footer">
+										Brelvis Hesley, <cite>Some guy at a wedding</cite>
+									</footer>
+								</blockquote>
+						</div>
+						<div class="col-lg-6 hidden-md-down">
+								<img src="img/brad-elvis.png" alt="Bad as elvis at a wedding">
+						</div>
+						
+					</div>
+				</div>
+		    </div>
+	  </section>
+	  
+	  <section id="feature-five" class="feature-full">
+	  		<div class="container">
+		  			<div class="row">
+						<div class="feature-content">
+							<div class="col-lg-8 col-lg-offset-2">
+								<h6>Powerfully packed action</h6>
+								<h2>When it absolutely, positively has to be Startup.logo overnight.</h2>
+								<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper neque quam, at facilisis nisl ornare ut. Ut ut tempor ligula, sed consectetur diam. Aliquam varius aliquam ipsum at gravida. Mauris a mauris finibus, pharetra purus in, imperdiet nisi. Praesent ultricies eu nunc nec cursus. 
+								</p>
+								
+								<h6>With Startup.logo you can:</h6>
+								<div class="row">
+									<div class="col-lg-6">
+											<ul>
+												<li>Put Sriracha on just about everything</li>
+												<li>Use your wallet for everything other than buying</li>
+											</ul>
+												 
+									</div>
+									<div class="col-lg-6">
+										
+										<ul>
+												<li>Become a multi-millionaire</li>
+												<li>Win at most things</li>
+											</ul>
+									</div>
+									
+									<img src="img/macbook-2.png" alt="macbbok">
+								</div>
+								
+							</div>
+						</div>
+					</div>
+		  	</div>
+	  
+	  </section>
+	  
+	  <section id="feature-six" class="feature-dark">
+		  <div class="cntainer">
+		  	<div class="row">
+			  	<div class="feature-content">
+					<div class="col-lg-6 hidden-md-down">
+							<img src="img/ipad.png" alt="ipad">
+					</div>
+					<div class="col-lg-5 col-lg-offset-1">
+						<h6>The all-in-one solution</h6>
+						<h2>Nobody doesn't like start-up.logo</h2>
+						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper neque quam, at facilisis nisl ornare ut. Ut ut tempor ligula, sed consectetur diam. Aliquam varius aliquam ipsum at gravida. Mauris a mauris finibus, pharetra purus in, imperdiet nisi. Praesent ultricies eu nunc nec cursus. 
+						</p>
+						<a href="" role="button" class="btn btn-secondary btn-lg">Mangoes!</a>
+					</div>
+				</div>
+			 </div>
+		  </div>
+	  
+	  </section>
+	  
+	  <section id="feature-seven">
+	  	<div class="container">
+		  	<div class="row">
+				<div class="feature-content">
+					<div class="col-lg-8 col-lg-offset-2">
+						<h6>100% No-money back guarantee</h6>
+						<h2>Money, Money, Money...omnomnomnom</h2>
+						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper neque quam, at facilisis nisl ornare ut. Ut ut tempor ligula, sed consectetur diam. Aliquam varius aliquam ipsum at gravida. Mauris a mauris finibus, pharetra purus in, imperdiet nisi. Praesent ultricies eu nunc nec cursus. 
+						</p>
+						<a href="" role="button" class="btn btn-success btn-lg">Toss some money our way (please)</a>
+					</div>
+				</div>
+			</div>
+		  
+		  </div>
+	  
+	  </section>
+	  
+	  <footer id="main-footer">
+	  		<div class="container">
+		    	<div class="row">
+					<div class="col-md-6">
+						&copy; 2016 Startup.logo is not a real thing. if it is, this site was not meant to be a mockery of it.
+						Coded by Jyotsna
+					</div>
+					<div class="col-md-2">
+						<h6>Our Product</h6>
+						<ul class="list-unstyled">
+							<li><a href="">Features</a></li>
+							<li><a href="">Solutions</a></li>
+							<li><a href="">Plans</a></li>
+							<li><a href="">Sign in</a></li>
+						</ul>
+					</div>
+					<div class="col-md-2">
+						<h6>Our Product</h6>
+						<ul class="list-unstyled">
+							<li><a href="">About</a></li>
+							<li><a href="">highlights</a></li>
+							<li><a href="">Investors</a></li>
+							<li><a href="">Partners</a></li>
+						</ul>
+					</div>
+					<div class="col-md-2">
+						<h6>Other Things</h6>
+						<ul class="list-unstyled">
+							<li><a href="">Careers</a></li>
+							<li><a href="">Contact</a></li>
+							<li><a href="">FAQ</a></li>
+							
+						</ul>
+					</div>
+				</div>
+		    </div>
+	  
+	  </footer>
+	  
+	  
+	  <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
+  </body>
+</html>
